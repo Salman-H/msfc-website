36352ffad0e9682e06e9326bfa07003a39aa4879
Dan Crisan <dancrisan@Dans-Air.lan>
2015-09-28 04:18:44 -0400

    fixed mailing server

---
+++ msfc-site/server.js
@@ -0,0 +1,55 @@
+/*
+Awsesome backend of a registration form.
+Why awesome?
+- source address is server side
+- source password is server side (duh...)
+- destinatary address is server side
+- node.js
+*/
+
+var express=require('express');
+var nodemailer = require("nodemailer");
+var app=express();
+
+/*
+Here we are configuring our SMTP Server details.
+STMP is mail server which is responsible for sending and recieving email.
+*/
+var smtpTransport = nodemailer.createTransport("SMTP",{
+	service: "Gmail",
+	auth: {
+		user: "imagineany@gmail.com",
+		pass: "Hunedoara04"
+	}
+});
+/*------------------SMTP Over-----------------------------*/
+
+
+/*------------------Routing Started ------------------------*/
+
+var port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/send',function(req,res){
+	var mailOptions={
+
+    	from: '' + req.query.name + '<dan.crisan@live.com>',
+        to : "dan.crisan@live.com",
+		subject : "My email is " + req.query.dest + " and...",
+		    // plaintext body
+    	text : 'You got a new registration!'
+    	//text: 'Hello to myself!'
+		//text : req.query.text
+	}
+	smtpTransport.sendMail(mailOptions);
+});
+
+/*--------------------Routing Over----------------------------*/
+
+//app.listen(port, function());
+//app.listen(80,80,80,80,80,80,80,80,'www.imagineany.com');
+//app.listen(8080, '45.55.217.187');
+app.listen(port, function(){
+    console.log("Express Started on Port 8080");
+});
